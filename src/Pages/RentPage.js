5a790df7bb4a39c709a044abb4a55c2122c145ca
Yejin0O0 <yyj11kr@naver.com>
2024-07-14 00:05:15 +0900

    Feat: RentPage에 CarCard 적용

--- src/Pages/RentPage.js
+++ src/Pages/RentPage.js
@@ -1,7 +1,12 @@
+import React from "react";
+import CarCard from "../Components/CarCard";
 import AppointmentContainer from "../Containers/AppointmentContainer";
 import BottomContainer from "../Containers/BottomContainer";
 import HeaderContainer from "../Containers/HeaderContainer";
-import { Div } from "../styles/assets/Div";
+import popularCarData from "../data/popular-car.json";
+import recommendationCarData from "../data/recommendation-car.json";
+import { Div, FlexDiv } from "../styles/assets/Div";
+import P from "../styles/assets/P";
 
 const RentPage = () => {
     return (
@@ -9,6 +14,68 @@ const RentPage = () => {
             <HeaderContainer />
             <Div width="100%" $backcolor="background" $padding="0 3%">
                 <AppointmentContainer />
+
+                <Div width="100%" $margin="50px 0 10px 0">
+                    <FlexDiv width="100%" $justifycontent="space-between" $margin="20px 0">
+                        <Div>
+                            <P color="secondary" $lineheight="21px" $fweight="600">
+                                Popular Car
+                            </P>
+                        </Div>
+                        <Div $pointer>
+                            <P color="primary" $lineheight="21px" $fweight="600">
+                                View All
+                            </P>
+                        </Div>
+                    </FlexDiv>
+                    <FlexDiv width="100%" $justifycontent="space-between">
+                        {popularCarData.slice(0, 4).map((car) => (
+                            <CarCard
+                                key={car.id} // key 추가
+                                name={car.name}
+                                type={car.type}
+                                image={car.image}
+                                fuelCapacity={car.fuelCapacity}
+                                gearType={car.gearType}
+                                seats={car.seats}
+                                isLiked={car.isLiked}
+                                originalPrice={car.originalPrice}
+                                salePrice={car.salePrice}
+                            />
+                        ))}
+                    </FlexDiv>
+                </Div>
+
+                <FlexDiv direction="column" width="100%">
+                    <FlexDiv width="100%" $justifycontent="space-between" $margin="20px 0">
+                        <Div>
+                            <P color="secondary" $lineheight="21px" $fweight="600">
+                                Recommendation Car
+                            </P>
+                        </Div>
+                    </FlexDiv>
+                    <FlexDiv width="100%" $justifycontent="space-between">
+                        {recommendationCarData.slice(0, 8).map((car) => (
+                            <CarCard
+                                key={car.id} // key 추가
+                                name={car.name}
+                                type={car.type}
+                                image={car.image}
+                                fuelCapacity={car.fuelCapacity}
+                                gearType={car.gearType}
+                                seats={car.seats}
+                                isLiked={car.isLiked}
+                                originalPrice={car.originalPrice}
+                                salePrice={car.salePrice}
+                            />
+                        ))}
+                    </FlexDiv>
+                    <FlexDiv height="44px" $radius="4" $padding="0 20px" $backcolor="primary" $pointer>
+                        <P $fweight="600" $lineheight="24px" color="wh">
+                            Show more car
+                        </P>
+                    </FlexDiv>
+                </FlexDiv>
             </Div>
             <BottomContainer />
         </Div>
