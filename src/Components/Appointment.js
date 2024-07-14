02aa09a68411d0fb84b0555e52dae80b57dd2a28
Yejin0O0 <yyj11kr@naver.com>
2024-07-14 20:37:03 +0900

    Feat: dropdown 내용 적용

--- src/Components/Appointment.js
+++ src/Components/Appointment.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 import { Div, FlexDiv } from "../styles/assets/Div";
 import P from "../styles/assets/P";
 import Dropdown from "./Dropdown";
@@ -14,6 +13,89 @@ const Point = styled.div`
 
 const Appointment = (props) => {
     const { pick } = props;
+    const locationList = [
+        "서울",
+        "부산",
+        "인천",
+        "대구",
+        "대전",
+        "광주",
+        "수원",
+        "울산",
+        "창원",
+        "성남",
+        "고양",
+        "용인",
+        "청주",
+        "전주",
+        "천안",
+        "포항",
+        "김해",
+        "제주",
+        "안양",
+        ,
+        "평택",
+    ];
+    const dateList = [
+        "08-01",
+        "08-02",
+        "08-03",
+        "08-04",
+        "08-05",
+        "08-06",
+        "08-07",
+        "08-08",
+        "08-09",
+        "08-10",
+        "08-11",
+        "08-12",
+        "08-13",
+        "08-14",
+        "08-15",
+        "08-16",
+        "08-17",
+        "08-18",
+        "08-19",
+        "08-20",
+        "08-21",
+        "08-22",
+        "08-23",
+        "08-24",
+        "08-25",
+        "08-26",
+        "08-27",
+        "08-28",
+        "08-29",
+        "08-30",
+        "08-31",
+    ];
+    const timeList = [
+        "00:00",
+        "01:00",
+        "02:00",
+        "03:00",
+        "04:00",
+        "05:00",
+        "06:00",
+        "07:00",
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00",
+    ];
+
     return (
         <Div width="582px" $justifycontent="space-between" $padding="30px 50px" $backcolor="wh" $radius="10">
             <FlexDiv $margin="0 0 20px 0">
@@ -34,7 +116,7 @@ const Appointment = (props) => {
                         </P>
                     </Div>
                     <Div>
-                        <Dropdown label="Select your city" />
+                        <Dropdown label="Select your city" options={locationList} />
                     </Div>
                 </Div>
                 <Div width="154px" height="48px" $borderr="1px solid lightGrey" $margin="0 0 0 13px">
@@ -44,7 +126,7 @@ const Appointment = (props) => {
                         </P>
                     </Div>
                     <Div width="126px">
-                        <Dropdown label="Select your date" />
+                        <Dropdown label="Select your date" options={dateList} />
                     </Div>
                 </Div>
                 <Div width="150px" height="48px" $margin="0 0 0 13px">
@@ -54,7 +136,7 @@ const Appointment = (props) => {
                         </P>
                     </Div>
                     <Div width="126px">
-                        <Dropdown label="Select your time" />
+                        <Dropdown label="Select your time" options={timeList} />
                     </Div>
                 </Div>
             </FlexDiv>
--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,23 +1,79 @@
+import { useState } from "react";
+import styled from "styled-components";
 import { Div, FlexDiv } from "../styles/assets/Div";
 import Img from "../styles/assets/Img";
 import P from "../styles/assets/P";
 
+const DropDownList = styled("ul")`
+    width: 100%;
+    height: 200px;
+    overflow: auto;
+    padding: 0;
+    margin: 0;
+    background: ${(props) => props.theme.color.wh};
+    box-sizing: border-box;
+    box-shadow: 0px 20px 50px 0 rgba(0, 0, 0, 0.2);
+
+    font-size: ${(props) => props.theme.fontSize.sm};
+    font-weight: 500;
+    &:first-child {
+        padding-top: 0.8em;
+    }
+`;
+
+const ListItem = styled("li")`
+    list-style: none;
+    margin-bottom: 0.8em;
+    padding: 10px 20px;
+    cursor: pointer;
+    &:hover {
+        background: ${(props) => props.theme.color.primary};
+        color: ${(props) => props.theme.color.wh};
+    }
+`;
+
 /*
     label: 선택되기 전 렌더링되는 문구
 */
-const Dropdown = ({ label }) => {
+const Dropdown = (props) => {
+    const { label, options } = props;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [selectedOption, setSelectedOption] = useState("");
+
+    const toggling = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const onOptionClicked = (option) => () => {
+        setSelectedOption(option);
+
+        setIsOpen(false);
+    };
+
     return (
         <Div width="100%" $position="relative">
-            <FlexDiv width="130px" height="20px" $pointer $justifycontent="space-between">
+            <FlexDiv width="130px" height="20px" $pointer $justifycontent="space-between" onClick={() => toggling()}>
                 <Div>
                     <P $fsize="xs" color="secondary">
-                        {label}
+                        {selectedOption || label}
                     </P>
                 </Div>
                 <Div width="14px" $margin="0 0 0 20px">
                     <Img src="/icons/chevron-down.svg" />
                 </Div>
             </FlexDiv>
+            {isOpen && (
+                <FlexDiv $zIndex={3} width="100%" $position="absolute">
+                    <DropDownList>
+                        {options.map((option, idx) => (
+                            <ListItem key={option + idx} onClick={onOptionClicked(option)}>
+                                {option}
+                            </ListItem>
+                        ))}
+                    </DropDownList>
+                </FlexDiv>
+            )}
         </Div>
     );
 };
